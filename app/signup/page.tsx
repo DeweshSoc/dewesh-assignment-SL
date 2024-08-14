2bb017e7c8f897ebd89438527fb1d092a24d691e
Dewesh Jha <deweshjha4@gmail.com>
2024-08-14 13:08:54 +0530

    add signup component

---
+++ app/signup/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Login } from "../ui/home";
+
+
+export default function Page() {
+
+    return (
+        <main className={styles.main}>
+            <Login></Login>
+        </main>
+    );
+}
