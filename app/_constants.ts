dd8fb490036e7188eefb0313c6ef5d945ad00962
Dewesh Jha <deweshjha4@gmail.com>
2024-08-14 19:35:21 +0530

    add constants

--- app/_constants.ts
+++ app/_constants.ts
@@ -1,4 +1,8 @@
 export const API_ENDPOINTS = {
     POST_CREATE: "/auth/signup",
-    POST_LOGIN:"/auth/login"
+    POST_LOGIN:"/auth/login",
+    POST_PROJECT_CREATE:"/project/create",
+    POST_FETCH_PROJECTS:"/project",
+    POST_FETCH_PROJECT_BY_ID:"/project/id"
+
 };
