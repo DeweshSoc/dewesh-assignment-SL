bff55f408f62f39f62e3879093004acf481b99b1
Dewesh Jha <deweshjha4@gmail.com>
2024-08-14 22:50:21 +0530

    add all http requests

--- app/lib/httpRequests.ts
+++ app/lib/httpRequests.ts
@@ -1,27 +1,108 @@
-// let url =
-//     "https://gdrvbncjfcqtydfbhfdm.hasura.eu-central-1.nhost.run/v1/graphql";
-
-// let options = {
-//     method: "POST",
-//     headers: {
-//         Accept: "application/json",
-//     },
-//     body: '{"query":"query GetUserTesting($shopifyUserId: String!, $shopifyOrderId: String!) {\n    obskit_backend_user_testing(where: { shopifyId: { _eq: $shopifyUserId }, shopifyOrderId: { _eq: $shopifyOrderId } }) {\n      shopifyId\n      shopifyOrderId\n    }\n  }\n","variables":{"shopifyUserId":"7770234028147","shopifyOrderId":"5918874894451"}}',
-// };
-
-// fetch(url, options)
-//     .then((res) => res.json())
-//     .then((json) => console.log(json))
-//     .catch((err) => console.error("error:" + err));
-
-
+const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NmJjY2Y3YWQwNmFiYjlmZWEzZjY4YTciLCJlbWFpbCI6Im1lZ2hhQHlvcG1haWwuY29tIiwiaWF0IjoxNzIzNjQ5OTQwLCJleHAiOjE3MjM4MjI3NDB9.jlUJ3yQqR_Rcrih4MjJsLEbv_1YP320GnIAb8NaDAts";
 
 export const sendPostRequest = (path:string, options:RequestInit) : Promise<any>=> {
 
     return new Promise(async(resolve, reject) => {
         try{
             const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
-            const res = await fetch(url, options);
+            const res = await fetch(url, {
+                ...options,
+                method:"POST",
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
+            });
+            if(!res.ok){
+                const resBodyStream = await res.json();
+                const msg = resBodyStream.message;
+                throw new Error(`Error ${res.status} - ${msg || res.statusText}`);
+
+            } 
+            const  response = res.json();
+            resolve(response);
+        }catch(err){
+            console.log(err);
+            reject(err);
+        }
+    })
+
+};
+export const sendPostRequestAuth = (path:string, options:RequestInit) : Promise<any>=> {
+
+    return new Promise(async(resolve, reject) => {
+        try{
+            const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
+            const res = await fetch(url, {
+                ...options,
+                method:"POST",
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if(!res.ok){
+                const resBodyStream = await res.json();
+                const msg = resBodyStream.message;
+                throw new Error(`Error ${res.status} - ${msg || res.statusText}`);
+
+            } 
+            const  response = res.json();
+            resolve(response);
+        }catch(err){
+            console.log(err);
+            reject(err);
+        }
+    })
+
+};
+
+export const sendGetRequest = (path:string, options:RequestInit) : Promise<any>=> {
+
+    return new Promise(async(resolve, reject) => {
+        try{
+            const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
+            const res = await fetch(url, {
+                ...options,
+                method:'GET',
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
+            });
+            if(!res.ok){
+                const resBodyStream = await res.json();
+                const msg = resBodyStream.message;
+                throw new Error(`Error ${res.status} - ${msg || res.statusText}`);
+
+            } 
+            const  response = res.json();
+            resolve(response);
+        }catch(err){
+            console.log(err);
+            reject(err);
+        }
+    })
+
+};
+
+
+export const sendGetRequestAuth = (path:string, options:RequestInit) : Promise<any>=> {
+
+    return new Promise(async(resolve, reject) => {
+        try{
+            const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
+            const res = await fetch(url, {
+                ...options,
+                method:'GET',
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
             if(!res.ok){
                 const resBodyStream = await res.json();
                 const msg = resBodyStream.message;
